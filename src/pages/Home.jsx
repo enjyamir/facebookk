bb18979653903e37f665a24ef424e8c39f778480
Enjy Amir <enjyamir@Enjys-MacBook-Pro.local>
2025-12-25 15:25:11 +0200

    final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,27 @@
+import React from 'react';
 import Navbar from '../components/Navbar';
 import LeftSidebar from '../components/LeftSidebar';
 import RightSidebar from '../components/RightSidebar';
 import Stories from '../components/Stories';
-import './Home.css';
-import CreatePost from '../components/CreatePost'; 
+import CreatePost from '../components/CreatePost';
 import Postcard from '../components/Postcard';
+import './Home.css';
 
-const Home = () => {
+function Home() {
   return (
-    <div className="home-page">
+    <div className="home">
       <Navbar />
-      <LeftSidebar />
-      <RightSidebar />
-      
-      <main className="main-content">
-        <div className="feed-container">
+      <div className="home__container" style={{ display: 'flex' }}>
+        <LeftSidebar />
+        <div className="home__feed" style={{ flex: 1 }}>
           <Stories />
           <CreatePost />
-          <PostCard 
-            username="Albert Flores"
-            initial="A"
-            timestamp="2 d"
-            imageText="Landscape Photo"
-          />
-          <PostCard 
-            username="Jenny Wilson"
-            initial="J"
-            timestamp="3 d"
-            imageText="Travel Photo"
-          />
+          <Postcard />
         </div>
-      </main>
+        <RightSidebar />
+      </div>
     </div>
   );
-};
+}
 
 export default Home;
